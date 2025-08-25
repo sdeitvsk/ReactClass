cb4fbc70ad84062c6cb5b5c877f35dd4c2e3c32a
SDE IT VSK <sdeitvsk@gmail.com>
2025-08-25 08:14:08 +0000

    day1-Ex1.1

--- day1/src/components/Card.jsx
+++ day1/src/components/Card.jsx
@@ -5,10 +5,14 @@ function Card(props){
     return (
          <div className="Card">
            <div > Name : {props.name}</div>  
-           <div > email : {props.email}</div>  
-           <div > age : {props.age}</div>  
-           <Gender gender={props.gender}/>
-
+           {props.email && <div > email : {props.email}</div>  }           
+           {props.age && <div > age : {props.age}</div>  }
+            <Gender gender={props.gender}/> 
+            <div>
+                {props?.add1?.line} <br />
+                {props?.add1?.dist} <br />
+                {props?.add1?.State}
+            </div>
          </div>
 
     )
